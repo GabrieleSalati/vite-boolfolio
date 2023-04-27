d913e875495050b32ec9ed53f41c4c3a8ab1de8c
GabrieleSalati <gabriele.salati5@gmail.com>
2023-04-27 13:54:26 +0200

    Axios call operational

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
 
+export default {
+  data() {
+    return {
+      title: "Hello world"
+    }
+  },
+
+  methods: {
+    fetchProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then((response) => { console.log(response) })
+    }
+  },
+
+  created() {
+    this.fetchProjects();
+  },
+
+  components: {
+    AppHeader: AppHeader,
+  },
+};
 </script>
 
 <template>
+  <AppHeader />
   <h1>{{ title }}</h1>
 </template>
 
