3619e57d2d40ac3c072eadafa5e5c6cda06b2488
GabrieleSalati <gabriele.salati5@gmail.com>
2023-05-01 18:47:30 +0200

    Multi page vue view operational

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,11 +9,11 @@ export default {
 
 <template>
     <div class="card">
-        <img :src="project.image" class="card-img-top" alt="Picture">
+        <!-- <img :src="project.image" class="card-img-top" alt="Picture"> -->
         <div class="card-body">
             <h5 class="card-title">{{ project.title }}</h5>
             <h6>{{ project.technology.label }}</h6>
-            <h6>{{ project.types.labels }}</h6>
+            <!-- <h6>{{ project.types.labels }}</h6> -->
             <p class="card-text">{{ project.description }}</p>
             <a href="#" class="btn btn-primary">Details</a>
         </div>
